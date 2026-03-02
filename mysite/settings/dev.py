161dc3a9d23dc2c4044274a4f1415627d43829bd
David Galvao <mrdavidalmeida@gmail.com>
2026-03-02 17:03:33 -0300

    Ambiente Wagtail + Docker + Postgre montado e rodando

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-^s2f9)_scz5g2w^y)51=#cr^x**1-*#3$+-9g@(0m66(bfmw+="
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
